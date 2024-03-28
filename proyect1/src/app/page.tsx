33d0d70f74cbb43d3f2df95da5012b58414921dc
Marisol Valverde <marisol.valverde.retana@est.una.ac.cr>
2024-03-27 19:26:56 -0600

    Basics changes

--- proyect1/src/app/page.tsx
+++ proyect1/src/app/page.tsx
@@ -1,9 +1,15 @@
 import Image from "next/image";
 
 export default function Home() {
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      Hola Mundo
-    </main>
-  );
+
+<main className="justify-center items-center inline-flex">
+  <div className="w-80 h-96 flex flex-col items-center justify-center min-h-screen bg-gradient-to-br from-purple-950 via-purple-900 to-purpleEnd">
+    HOLAAAAAAAAAA
+  </div>
+</main>
+
+ 
+ );
 }
