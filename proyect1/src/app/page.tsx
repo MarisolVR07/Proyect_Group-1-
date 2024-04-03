48352a142a102186e921e458e03eb56acd4a92e9
Jumaikel Chinchilla Mora <jumaikel.chinchilla.mora@est.una.ac.cr>
2024-04-03 16:27:57 -0600

    Changes to the login interface

--- proyect1/src/app/page.tsx
+++ proyect1/src/app/page.tsx
@@ -5,11 +5,9 @@ export default function Home() {
 
   return (
 
-  <main className="w-full h-full flex flex-col items-center justify-center min-h-screen bg-gradient-to-br from-black via-100% via-violet-900  to-violet-800">
-  <div>
-    <Register/>
-  </div>
-  </main>
- 
- );
+    <main className="w-full h-full flex flex-col items-center justify-center min-h-screen bg-gradient-to-br from-black via-black   to-violet-900">
+      <Login />
+    </main>
+
+  );
 }
--- proyect1/src/components/Login.tsx
+++ proyect1/src/components/Login.tsx
@@ -2,46 +2,47 @@ import React from "react";
 
 const Login = () => {
   return (
-    <div className="bg-slate-700 p-6 rounded-xl font-poppins font-semibold w-full max-w-sm drop-shadow-xl">
-      <div className="form-control w-full max-w-xs">
-        <div className="">
-          <div className="flex items-center justify-center">
-            <svg
-              className="h-32 w-32 text-slate-900 bg-white rounded-full shadow-xl"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"
-              />
-            </svg>
-          </div>
-        </div>
+    <div className="form-control p-6 w-full items-center justify-center bg-slate-700 rounded-2xl font-poppins font-semibold max-w-md drop-shadow-xl text-center">
+      <div>
+        <svg
+          className="h-32 w-32 text-black bg-slate-100 rounded-full shadow-xl"
+          fill="none"
+          viewBox="0 0 24 26"
+          stroke="currentColor"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"
+          />
+        </svg>
+      </div>
+      <h1 className="mt-3 text-3xl text-white">LOGIN</h1>
+      <div className="mb-6 w-full">
         <label className="label">
-          <span className="label-text text-white">Correo:</span>
+          <span className="label-text text-white">Email:</span>
         </label>
         <input
           type="email"
           placeholder="example@example.com"
-          className="input input-bordered w-full max-w-xs mb-4"
+          className="input bg-white text-slate-700 input-bordered w-full h-8"
         />
+      </div>
+      <div className="w-full">
         <label className="label">
-          <span className="label-text text-white">Contraseña:</span>
+          <span className="label-text text-white">Password:</span>
         </label>
         <input
           type="password"
-          placeholder="Your password"
-          className="input input-bordered w-full max-w-xs mb-4"
+          placeholder="your password"
+          className="input bg-white text-slate-700 input-bordered w-full h-8"
         />
       </div>
-      <button className="btn text-white btn-primary w-full max-w-xs">
-        Iniciar Sesión
+      <button className=" text-white bg-violet-800 hover:bg-violet-700 rounded-md h-8 w-44 mt-7 mb-16">
+        SING IN
       </button>
-    </div>
+    </div >
   );
 };
 
