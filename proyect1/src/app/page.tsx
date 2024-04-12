972f630910307aa002a2b55dc3eda0c2b11abffe
Marisol Valverde <marisol.valverde.retana@est.una.ac.cr>
2024-04-11 23:08:41 -0600

    Register Deleted

--- proyect1/src/app/page.tsx
+++ proyect1/src/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
 
   <main className="w-full h-full flex flex-col items-center justify-center min-h-screen bg-gradient-to-br from-black via-100% via-violet-900  to-violet-800">
   <div>
-    <Register/>
+    <Login/>
   </div>
   </main>
  
