b3027c160839632b8323fb17b759513d5b3188e1
Joksanmj72 <joksanmj.777@gmail.com>
2024-04-28 17:51:36 -0600

    UPDATED msConfig.ts

--- proyect1/src/components/msalConfig.ts
+++ proyect1/src/components/msalConfig.ts
@@ -5,7 +5,7 @@ import { Configuration , PublicClientApplication } from '@azure/msal-browser';
 export const msalConfig: Configuration = {
   auth: {
     clientId: '3149a216-293c-4ae9-a1a4-ef4651b935b0',
-    authority: 'https://login.microsoftonline.com/common',
+    authority: 'https://login.microsoftonline.com/common/oauth2/v2.0/authorize',
     redirectUri: 'http://localhost:3000', 
     
 
