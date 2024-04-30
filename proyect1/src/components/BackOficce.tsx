c3ae28e5f77c208bcff5ebcdfeb2bacb67546e5a
unknown <mariangel.solis.bils@gmail.com>
2024-04-29 20:06:37 -0600

    changes on views, header

--- proyect1/src/components/BackOficce.tsx
+++ proyect1/src/components/BackOficce.tsx
@@ -1,91 +1,13 @@
 "use client";
-import Header from "@/components/Header";
-import Button from "@/components/PrimaryButton";
 import React, { useState } from "react";
-import Link from 'next/link';
-
+import Header from "@/components/Header";
 
 export default function Page() {
-  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
-  const toggleDropdown = () => {
-    setIsDropdownOpen(!isDropdownOpen);
-  };
-
-  const handleEvaluationsClick = () => {
-    console.log("Evaluatons");
-  };
-
-  const handleUsersClick = () => {
-    console.log("Users");
-  };
-
-  const handleReportsClick = () => {
-    console.log("Reports");
-  };
+  
 
   return (
     <main className="relative w-full flex flex-col items-center justify-center min-h-screen bg-gradient-to-t from-violet-950 to-black pt-24">
-      <Header />
-      <div
-        className="absolute top-20 mx-auto my-8 bg-gray-700 bg-opacity-50 backdrop-blur-sm rounded-2xl w-1/2 p-6 flex flex-col items-center"
-        style={{ minHeight: "32rem" }}
-      >
-        <h1 className="text-white text-3xl font-bold text-center mb-7">
-          BackOffice
-        </h1>
-
-        <div className="w-full flex flex-col items-center">
-          <Link href="/backoffice/Institution/" passHref>
-            <Button className="mb-2 w-full">
-            Institution
-            </Button>
-          </Link>
-          <Button onClick={toggleDropdown} className="mb-2 w-full">
-            Departments
-          </Button>
-          <div className="relative w-full">
-            {isDropdownOpen && (
-              <div className="absolute z-10 w-full bg-white rounded-md shadow-lg">
-                <a
-                  href="#"
-                  className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
-                >
-                  Department 1
-                </a>
-                <a
-                  href="#"
-                  className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
-                >
-                  Department 2
-                </a>
-                <a
-                  href="#"
-                  className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
-                >
-                  Department 3
-                </a>
-              </div>
-            )}
-          </div>
-          <Link href="/backoffice/create-self-assessment" passHref>
-            <Button className="mb-2 w-full">
-              Self-Assessment
-            </Button>
-          </Link>
-          <Link href="/backoffice/users" passHref>
-            <Button className="mb-2 w-full">
-              Users
-            </Button>
-          </Link>
-
-          <Button
-            onClick={() => console.log("Reports")}
-            className="mb-2 w-full"
-          >
-            Reports
-          </Button>
-        </div>
-      </div>
+   <Header />
     </main>
   );
 }
--- proyect1/src/components/Header.tsx
+++ proyect1/src/components/Header.tsx
@@ -1,16 +1,40 @@
+'use client'
+import React, { useState } from 'react';
+import Link from 'next/link';
+import PrimaryButton from './PrimaryButton';
+import SecondaryButton from './PrimaryButton';
 export default function Header() {
+    const [isDropdownOpen, setDropdownOpen] = useState(false);
+
+    const toggleDropdown = () => setDropdownOpen(!isDropdownOpen);
+
     return (
-        <header className="w-full bg-transparent text-white py-4 px-8 top-0 z-10">
-            <div className="container mx-auto flex justify-between items-center">
-                <div className="items-center">
+        <header className="w-full bg-transparent text-white fixed py-4 px-8 top-0 z-10">
+        <div className="container mx-auto flex justify-between items-center">
+              <div className="items-center">
                     <div className="text-4xl font-bold mr-4">SCI</div>
                     <div className="text-xs">Internal System Control</div>
                 </div>
                 <nav>
                     <ul className="flex space-x-4">
-                        <li className="hover:bg-violet-700 px-3 py-2 rounded-md">Institution</li>
-                        <li className="hover:bg-violet-700 px-3 py-2 rounded-md">Users</li>
-                        <li className="hover:bg-violet-700 px-3 py-2 rounded-md">Reports</li>
+                    <SecondaryButton className="hover:bg-violet-700 px-3 py-2">DashBoard</SecondaryButton>
+                        <li className="hover:bg-violet-700 px-3 py-2 rounded-md">
+                            <PrimaryButton onClick={toggleDropdown}>BackOffice</PrimaryButton>
+                            {isDropdownOpen && (
+                                <ul className="absolute bg-white text-black py-2 rounded-md ">
+                                     <Link href="/backoffice/Institution/" passHref>
+                                    <li className="hover:bg-violet-700 px-3 py-2">Institution</li>
+                                    </Link>
+                                    <Link href="/backoffice/create-self-assessment" passHref>
+                                    <li className="hover:bg-violet-700 px-3 py-2">Self-Assessment</li>
+                                    </Link>
+                                    <Link href="/backoffice/users" passHref>
+                                    <li className="hover:bg-violet-700 px-3 py-2">Users</li>
+                                    </Link>
+                                </ul>
+                            )}
+                        </li>
+                        <SecondaryButton className="hover:bg-violet-700 px-3 py-2 rounded-md">Reports</SecondaryButton>
                     </ul>
                 </nav>
             </div>
