a9e7d5cd9552f0fdfc9b800b5df3c69e3ca7d70e
unknown <mariangel.solis.bils@gmail.com>
2024-05-12 17:40:38 -0600

    CREATE store de departments
    adding store

---
+++ proyect1/src/store/departmentStore.ts
@@ -0,0 +1,85 @@
+import { create } from "zustand";
+import {
+    deleteDepartment,
+    getDepartment,
+    saveDepartment,
+    getDepartments,
+    updateDepartment,
+} from "@/app/controllers/rc_departments/controller";
+import { Department } from "@/app/types/entities";
+import { ErrorResponse } from "@/app/types/api";
+
+interface DepartmentState {
+    departments: Department[];
+    currentDepartment: Department | null;
+    getDepartment: (id: number) => Promise<Department | ErrorResponse>;
+    deleteDepartment: (
+      id: number
+    ) => Promise<Department | ErrorResponse>;
+    saveDepartment: (
+        department: Department
+    ) => Promise<Department | ErrorResponse>;
+    getDepartments: () => Promise<Department[] | ErrorResponse>;
+    updateDepartment: (
+        department: Department
+    ) => Promise<Department | ErrorResponse>;
+  }
+
+  
+export const useDepartmentsStore = create<DepartmentState>((set) => ({
+    departments: [],
+    currentDepartment: null,
+    getDepartment: async (id: number) => {
+      const department = await getDepartment(id);
+      if ("error" in department) {
+        return department;
+      }
+      set((state) => ({ ...state, departments: [department] }));
+      return department;
+    },
+    deleteDepartment: async (id: number) => {
+      const department = await deleteDepartment(id);
+      if ("error" in department) {
+        return department;
+      }
+      set((state) => ({
+        ...state,
+        departments: state.departments.filter((dep) => dep.DPT_Id !== id),
+      }));
+      return department;
+    },
+    saveDepartment: async (department: Department) => {
+      const newDepartment = await saveDepartment(department);
+      if ("error" in newDepartment) {
+        return newDepartment;
+      }
+      set((state) => ({
+        ...state,
+        departments: [...state.departments, newDepartment],
+        currentDepartment: newDepartment,
+      }));
+      return newDepartment;
+    },
+    getDepartments: async () => {
+      const departments = await getDepartments();
+      if ("error" in departments) {
+        return departments;
+      }
+      set((state) => ({ ...state, departments }));
+      return departments;
+    },
+    updateDepartment: async (department: Department) => {
+      const updatedDepartment = await updateDepartment(department);
+      if ("error" in updatedDepartment) {
+        return updatedDepartment;
+      }
+      set((state) => ({
+        ...state,
+        departments: state.departments.map((dep) =>
+            dep.DPT_Id === department.DPT_Id? updatedDepartment : dep
+        ),
+      }));
+      return updatedDepartment;
+    },
+  }));
+  
