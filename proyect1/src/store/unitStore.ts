d905f5f712d5707c6383e28cf5f8419ee4f67645
Joksanmj72 <joksanmj.777@gmail.com>
2024-05-18 09:32:42 -0600

    UPDATE unitStore.ts
    I completed the store with useUnitStore hook

--- proyect1/src/store/unitStore.ts
+++ proyect1/src/store/unitStore.ts
@@ -25,4 +25,60 @@ interface UnitState {
     ) => Promise<Unit | ErrorResponse>;
   }
 
+  export const useUnitStore = create<UnitState>((set) => ({
+    units: [],
+    currentUnit: null,
+    getUnit: async (id: number) => {
+      const unit = await getUnit(id);
+      if ("error" in unit) {
+        return unit;
+      }
+      set((state) => ({ ...state, units: [unit] }));
+      return unit;
+    },
+    deleteUnit: async (id: number) => {
+      const unit = await deleteUnit(id);
+      if ("error" in unit) {
+        return unit;
+      }
+      set((state) => ({
+        ...state,
+        departments: state.units.filter((un) => un.UND_Id !== id),
+      }));
+      return unit;
+    },
+    saveUnit: async (unit: Unit) => {
+      const newUnit = await saveUnit(unit);
+      if ("error" in newUnit) {
+        return newUnit;
+      }
+      set((state) => ({
+        ...state,
+        units: [...state.units, newUnit],
+        currentUnit: newUnit,
+      }));
+      return newUnit;
+    },
+    getUnits: async () => {
+      const units = await getUnits();
+      if ("error" in units) {
+        return units;
+      }
+      set((state) => ({ ...state, units }));
+      return units;
+    },
+    updateUnit: async (unit: Unit) => {
+      const updatedUnit = await updateUnit(unit);
+      if ("error" in updatedUnit) {
+        return updatedUnit;
+      }
+      set((state) => ({
+        ...state,
+        units: state.units.map((un) =>
+            un.UND_Id === unit.UND_Id? updatedUnit : un
+        ),
+      }));
+      return updatedUnit;
+    },
+  }));
   
