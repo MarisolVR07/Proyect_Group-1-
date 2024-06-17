82626370b4af2f6d540c50aba2349a9b96fd3321
Marisol Valverde <marisol.valverde.retana@est.una.ac.cr>
2024-06-17 14:01:39 -0600

    Link to vercel

--- src/app/msalConfig.ts
+++ src/app/msalConfig.ts
@@ -4,7 +4,7 @@ export const msalConfig: Configuration = {
   auth: {
     clientId: "3149a216-293c-4ae9-a1a4-ef4651b935b0",
     authority: "https://login.microsoftonline.com/common/oauth2/v2.0/authorize",
-    redirectUri: "http://localhost:3000",
+    redirectUri: "https://proyect-group-1-9i313t9as-marisolvr07s-projects.vercel.app/",
   },
 };
 
